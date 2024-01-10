4b0bc45511462b1719acb86267750ee0ea0a4faf
elaljo <moelalj@c1r3p6.1337.ma>
2024-01-10 15:06:31 +0100

    creat folders

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 15:03:38 by moelalj           #+#    #+#             */
+/*   Updated: 2024/01/10 15:06:02 by moelalj          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie{
+	private:
+		std::string zombie_name;
+	public:
+		void	announce(void);
+		~Zombie();
+};
+#endif
