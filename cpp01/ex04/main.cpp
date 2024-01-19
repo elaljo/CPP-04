b79ea77f2c7dcde7b0bb4766eaa359367a6b4abb
elaljo <moelalj@c1r4p10.1337.ma>
2024-01-19 15:47:16 +0100

    done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,48 +6,45 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:53:36 by moelalj           #+#    #+#             */
-/*   Updated: 2024/01/19 10:10:05 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/01/19 15:45:56 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-std::string replace(std::string line, std::string s1, std::string s2)
-{
-	int	i = 0;
-
-	while (line[i])
-	{
-		if (line[i] == s1[0])
-			line[i] = s2[0];
-		i++;
-	}
-	return (line);
-}
 int main(int argc, char* argv[])
 {
 	if (argc == 4)
 	{
 		std::ifstream inputfile;
 		
-		inputfile.open(argv[1]);
+		inputfile.open(argv[1]); 
 		if (inputfile.is_open())
 		{
 			std::string line;
 			std::ofstream outputfile;
 			std::string replace_file = argv[1];
+			std::string s1 = argv[2];
+			std::string s2 = argv[3];
+			std::size_t	found;
 			
 			replace_file.append(".replace");
 			outputfile.open(replace_file);
 			if (outputfile.is_open())
 			{
-				std::string new_line;
-				
 				while (std::getline(inputfile, line))
 				{
-					new_line = replace(line, argv[2], argv[3]);
-					outputfile << new_line;
+					while (line.find(s1) != std::string::npos)
+					{
+						found = line.find(s1);
+						if (found != std::string::npos)
+						{
+							line = line.erase(found, s1.length());
+							line = line.insert(found, s2);
+						}
+					}
+					outputfile << line;
 					outputfile << "\n";
 				}
 				outputfile.close();
