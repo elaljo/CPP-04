4f557ce6800004726feb2e2d203d209c48a37455
elaljo <moelalj@c2r3p8.1337.ma>
2024-01-27 23:50:18 +0100

    added members + constructors para

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 23:32:36 by moelalj           #+#    #+#             */
-/*   Updated: 2024/01/27 23:34:43 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/01/27 23:48:27 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,13 @@ class Fixed{
 		static const int save_fractional_bits = 8;
 	public:
 		Fixed();
-		Fixed(const int);
+		Fixed(const int number);
 		Fixed(const float);
 		Fixed(const Fixed& copy);
 		Fixed& operator=(const Fixed& copy);
 		~Fixed();
+		float toFloat(void) const;
+		int toInt(void) const;
 		int getRawBits(void) const;
 		void	setRawBits(int const raw);
 };
