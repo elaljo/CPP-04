c94249742d0f0a6cbea07ad9432e755c50adf3e5
elaljo <moelalj@c2r4p2.1337.ma>
2024-02-05 15:23:05 +0100

    all is good

--- cpp04/ex00/Animal.cpp
+++ cpp04/ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 23:34:48 by moelalj           #+#    #+#             */
-/*   Updated: 2024/02/05 13:23:36 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/02/05 15:18:46 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ Animal::Animal() : type("Animal")
 {
 	std::cout << "Animal constructor called" << std::endl;
 }
-
 Animal::~Animal()
 {
 	std::cout << "Animal destructor called" << std::endl;
 }
- 
+
 void	Animal::makeSound() const
 {
 	std::cout << "i'am the boss no sound" << std::endl;
--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moelalj <moelalj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 23:41:24 by moelalj           #+#    #+#             */
-/*   Updated: 2024/02/05 13:46:51 by moelalj          ###   ########.fr       */
+/*   Updated: 2024/02/05 15:19:42 by moelalj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int main()
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	const WrongAnimal *w = new WrongCat();
-	//std::cout << j->getType() << " " << std::endl;
-	//std::cout << i->getType() << " " << std::endl;
-	//i->makeSound(); //will output the cat sound!
-	//j->makeSound();
-	//meta->makeSound();
-	w->makeSound();
-	delete meta;
+	//const WrongAnimal *w = new WrongCat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	//w->makeSound();
+	//delete meta;
 	delete i;
 	delete j;
 	
